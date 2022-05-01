ad8b890a0276663cabfe385509e5e917061db685
Athul Muralidhar <athul.m68@gmail.com>
2022-05-01 16:17:04 +0200

    working index page

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 // general source
 // https://blog.logrocket.com/rust-web-apps-using-rocket-framework/
 
+#![feature(decl_macro)]
+use rocket_contrib::templates::Template;
+use rocket::get;
+use rocket::Request;
+use rocket::response::content::Json;
+use rocket::request::Form;
+use rocket::catch;
+use rocket::routes;
+use rocket::catchers;
+use serde::Serialize;
+
 
 struct Book {
     title: String,
@@ -8,11 +19,41 @@ struct Book {
     isbn: String,
 }
 
-#[get("/")]
-fn index() -> Template {
-
+#[derive(Serialize)]
+struct Context {
+    first_name: String,
+    last_name: String
 }
 
+#[get("/")]
+fn index() -> Template {
+    let context = Context {
+        first_name: String::from("hardcoded firstname"),
+        last_name: String::from("hardcoded last name")
+    };
+    Template::render("home", context)
+}
+
+#[get("/hello")]
+fn api_hello() -> Json<& 'static str> {
+    Json("{
+        'status': 'success',
+    'message': 'Hello API!'
+    }")
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    println!("{}", req);
+    format!("not found: {}",req.uri())
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+    .register(catchers![not_found])
+    .mount("/", routes![index])
+    .mount("/api", routes![api_hello])
+    .attach(Template::fairing())
+    .launch();
 }
